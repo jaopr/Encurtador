a34c604b1d388c2b7cc45e39cf8581622cf1ead3
jaopr <joaopelite25@gmail.com>
2024-07-03 20:33:50 -0300

    Configurando o app Flask e o SQLAlchemy

--- encurtador.py
+++ encurtador.py
@@ -2,3 +2,7 @@ from flask import Flask, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import string, random
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
