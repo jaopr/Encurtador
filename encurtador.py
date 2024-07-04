f082a89fcf87d61ed7bcf03b7f2f78b2e353cb8c
jaopr <joaopelite25@gmail.com>
2024-07-03 21:33:09 -0300

    Adicionando as Rotas do Flask para encurtar a url e receber a url por um formulário

--- encurtador.py
+++ encurtador.py
@@ -19,3 +19,19 @@ def generate_short_url(): #definindo a função que encurta as URLs
         short_url = ''.join(random.choice(characters) for _ in range(10)) # Cria um string aleatória de 10 Caracteres
         if not URL.query.filter_by(short_url=short_url).first(): #Verifica se a URL é única
             return short_url
+
+        @app.route('/<short_url)') # Criando uma rota do Flask utilizando a função short_url
+        def redirect_to_url(short_url): # A função é chamada quando alguém acessa a rota
+            url = URL.query.filter_by(short_url = short_url).first_or_404() # Inicia uma consulta na tabela URL, filtra os resultados do campo short_url e executa a consulta, retornando ou o primeiro resultado encontrado ou o erro 404
+            return redirect(url.original_url)
+
+        @app.route('/shorten', methods=['POST']) #Define a rota short, só podendo ser acessado pelo método HTTP POST, ou seja, só aceitará solicitações enviadas pelo servidor.
+        def shorten_url():
+            original_url = request.form['original_url'] #recebe a solicitação HTTP
+            short_url = generate_short_url() # Chama a função generate_short_url para gerar uma url única.
+            new_url = URL(original_url = original_url, short_url = short_url) # Cria um novo objeto URL. a orignal_url é recebida no formulário e o short_url gera uma nova e aleatória.
+            db.session.add(new_url) #prepara a inserção do objeto no BD
+            db.session.commit() #Commita a alteração
+            return f'Shortened URL is: {short_url}' #Retorna a url gerada
+
+
